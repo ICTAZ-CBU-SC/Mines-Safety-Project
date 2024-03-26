c57ce704819ee9a9b89296ebd4f773972b69a1f3
joshua-inf <jsibanda407@gmail.com>
2024-03-26 21:50:04 +0200

    push map

--- Web app/src/App.js
+++ Web app/src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './Main';
 import NavBar from './components/Headers_and_footers/Nav';
+import './App.css'
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <div className=''>
+      <div className='d-flex'>
         <NavBar/>
-        <div className=''>
+        <div className='w-100'>
           <Routes>
             <Route path='/' element={<MainPage/>} />
           </Routes>
--- Web app/src/components/Headers_and_footers/Nav.js
+++ Web app/src/components/Headers_and_footers/Nav.js
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import { HiMenuAlt2 } from "react-icons/hi";
 
 const NavBar = () => {
+    const [slider, setSlider] = useState(false)
+    let slidenav = document.getElementById('slidenav')
+
+
+    const ActivateSlider = () => {
+        setSlider(!slider)
+
+        if(slider){
+            document.getElementById('slidenav').style.width = '0px';
+        }
+    }
+
+    
     return (
         <>
             <div>
-                <div style={{}}>
-                    <HiMenuAlt2 />
+                <div style={{position:'relative'}}>
+                    <div className="slidenav" style={{height:'100vh', width:'150px'}}>
+
+                    </div>
+                    <div style={{position:'absolute', top:'10px', right:'10px'}}>
+                        <HiMenuAlt2 onClick={() => ActivateSlider()} size={40} />
+                    </div>
                 </div>
             </div>
         </>
